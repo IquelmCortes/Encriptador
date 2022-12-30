34d8edd57a22eae82776b51608969bf1c844df75
developers-at-873263238094 <pedro.cortes@insoftar.com>
2022-12-30 16:44:52 -0500

    v1 de encriptador

---
+++ JS/index.js
@@ -0,0 +1,90 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            lg: ['24px', '28.8px'],
+            xl: ['32px', '48px'],
+        },
+        borderRadius: {
+            '3xl': ['24px'],
+            '4xl': ['32px']
+        },
+        extend: {
+            height: {
+                '67': '4.1875rem',
+            },
+            colors: {
+                blueCian: '#F3F5FC',
+                darkBlue: '#0A3871',
+            }
+        }
+    }
+}
+
+function vaciar() {
+    $("#EText").val("");
+}
+
+function encriptar() {
+    var texto = $("#EText").val();
+    if (texto != "") {
+        if (texto.includes("á") || texto.includes("é") || texto.includes("í") || texto.includes("ó") || texto.includes("ú") || texto.includes("/") || texto.includes("@") || texto.includes("#") || texto.includes("$") || texto.includes("%") || texto.includes("!") || texto.includes("¡") || texto.includes("?") || texto.includes("¿")) {
+            document.getElementById("bg-modal").style.display = "flex";
+            document.getElementById("modal-text").innerHTML = "No puede ingresar acentos";
+            document.getElementById("advertencia").style.color = "red"
+        } else {
+            document.getElementById("avatar").style.display = "none";
+            document.getElementById("not-found").style.display = "none";
+            document.getElementById("enter-text").style.display = "none";
+            var textE = texto.replaceAll("e", "enter");
+            var textI = textE.replaceAll("i", "imes");
+            var textA = textI.replaceAll("a", "ai");
+            var textO = textA.replaceAll("o", "ober");
+            var textU = textO.replaceAll("u", "ufat");
+            document.getElementById("copy").style.display = "initial";
+            document.getElementById("transcription").innerHTML = textU;
+            vaciar();
+        }
+    } else {
+        document.getElementById("bg-modal").style.display = "flex";
+        document.getElementById("modal-text").innerHTML = "Debe ingresar un texto";
+        document.getElementById("advertencia").style.color = "red"
+    }
+}
+
+function desencriptar() {
+    var texto = $("#EText").val();
+    if (texto != "") {
+        if (texto.includes("á") || texto.includes("é") || texto.includes("í") || texto.includes("ó") || texto.includes("ú") || texto.includes("/") || texto.includes("@") || texto.includes("#") || texto.includes("$") || texto.includes("%") || texto.includes("!") || texto.includes("¡") || texto.includes("?") || texto.includes("¿")) {
+            document.getElementById("bg-modal").style.display = "flex";
+            document.getElementById("modal-text").innerHTML = "No puede ingresar acentos";
+            document.getElementById("advertencia").style.color = "red"
+        } else {
+            document.getElementById("avatar").style.display = "none";
+            document.getElementById("not-found").style.display = "none";
+            document.getElementById("enter-text").style.display = "none";
+            var textE = texto.replaceAll("enter", "e");
+            var textI = textE.replaceAll("imes", "i");
+            var textA = textI.replaceAll("ai", "a");
+            var textO = textA.replaceAll("ober", "o");
+            var textU = textO.replaceAll("ufat", "u");
+            document.getElementById("copy").style.display = "initial";
+            document.getElementById("transcription").innerHTML = textU;
+            vaciar();
+        }
+    } else {
+        document.getElementById("bg-modal").style.display = "flex";
+        document.getElementById("modal-text").innerHTML = "Debe ingresar un texto";
+        document.getElementById("advertencia").style.color = "red"
+    }
+}
+
+function copiar() {
+    var content = document.getElementById("transcription").innerText;
+    navigator.clipboard.writeText(content);
+}
+
+function cerrarModal() {
+    document.getElementById("bg-modal").style.display = "none";
+    document.getElementById("advertencia").style.color = "initial"
+    vaciar();
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Este proyecto es una aplicación que puede cifrar y descifrar textos por medio de un capturador de textos.
+
+El proyecto hace parte de un reto de Alura en mi proceso dentro del programa One de Oracle.
+Por medio de esta aplicación planeo mostrar mis aptitudes hasta el momento en el manejo de HTML, Javascript y para los estilos de la página he implementado el uso de Tailwind.
+
+La aplicación busca verse de manera correcta en dispositivos móviles, tabletas y computadores, cumpliendo así el diseño responsive y empleando la filosofía "first mobile".
+
+Por favor ingrese texto sin acentos ni caracteres especiale.
+
+El diseño de esta página responde al recomendado por alura con unos pequeños cambios que creo que son parte de la mejora.
+
+Si ve alguna posibilidad de mejora, la recibiré con mucho gusto.
